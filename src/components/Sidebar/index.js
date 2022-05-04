4330127ae2db3241dce21784d354270f47491b76
ramirezandradesara <sararamirezandrade@gmail.com>
2022-05-04 00:10:28 -0300

    done

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,47 +1,72 @@
-import './index.scss';
-import { Link, NavLink } from 'react-router-dom';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-    return (
-        <div className="nav-bar">
-            <Link className='logo' to='/'>
-                <img src={LogoS} alt="logo" />
-                <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
-            </Link>
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
 
-            <nav>
-                {/* agregamos className para hover */}
-                <NavLink exact= "true" activeclassname="active" to='/'>
-                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
-                </NavLink>
-                <NavLink exact= "true" activeclassname="active"  className="about-link" to='/about'> 
-                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
-                </NavLink>
-                <NavLink exact= "true" activeclassname="active" className="contact-link" to='/contact'>
-                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
-                </NavLink>
-            </nav>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
 
-            <ul>
-                <li>
-                    <a href="https://www.linkedin.com/in/sara-ramirez-39b3081ab/" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />    
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/ramirezandradesara" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />    
-                    </a>
-                </li>
-            </ul>
-        </div>
-
-    )
+        <NavLink activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bobangajicsm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
-export default Sidebar;
+export default Sidebar
